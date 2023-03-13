8689737d6c15348299f18235b145a5628632bcdb
chopra1 <97356733+chopra1@users.noreply.github.com>
2023-03-13 12:53:00 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,21 @@
+//Write your age
+const myAge = 22;
+
+var earlyYears = 2;
+
+earlyYears *= 10.5; 
+
+let laterYears = myAge - 2;
+
+laterYears *= 4;
+
+//console.log(earlyYears);
+//console.log(laterYears);
+
+myAgeInDogYears = earlyYears + laterYears;
+//console.log(myAgeInDogYears);
+
+myName = "Chetan";
+console.log(myName.toLowerCase());
+
+console.log(`My name is ${myName}. I am ${myAgeInDogYears} years old in dog years.`);
